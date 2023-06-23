1f4414390a8fc0d11f2818ebfa5c8ef369ef61c3
Janani Elangovan <61153365+januhub@users.noreply.github.com>
2023-06-23 11:21:52 +0530

    Count number of vowels in a string

---
+++ Day9.js
@@ -0,0 +1,21 @@
+// program to count the number of vowels in a string
+// defining vowels
+
+const vowels = ["a", "e", "i", "o", "u"]
+function countVowel(str) {
+    // initialize count
+    let count = 0;
+    if (typeof myVar === 'string' || myVar instanceof String)
+        // loop through string to test if each character is a vowel
+        for (let letter of str.toLowerCase()) {
+            if (vowels.includes(letter)) {
+                count++;
+            }
+        }
+    // return number of vowels
+    return count
+}
+// take input
+const string = prompt('Enter a string: ');
+const result = countVowel(string);
+console.log(result);
