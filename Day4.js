b6c778e095b2906b4648175884c08eb99d5dbd3c
Janani Elangovan <61153365+januhub@users.noreply.github.com>
2023-06-23 10:38:57 +0530

    Create Day4.js

---
+++ Day4.js
@@ -0,0 +1,28 @@
+/*Create a class function which performs the below action
+p1 = Person("Samuel", 24)
+p2 = Person("Joel", 36)
+p3 = Person("Lily", 24)
+p1.compareAge(p2) ➞ "Joel is older than me."
+p2.compareAge(p1) ➞ "Samuel is younger than me."
+p1.compareAge(p3) ➞ "Lily is the same age as me."
+*/
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    compareAge(other) {
+        if (this.age > other.age) {
+            return `${other.name} is younger than me.`
+        } else if (this.age < other.age) {
+            return `${other.name} is older than me.`
+        } else {
+            return `${other.name} is the same age as me.`
+        }}}
+p1 = new Person("Samuel", 24)
+p2 = new Person("Joel", 36)
+p3 = new Person("Lily", 24)
+console.log(p1.compareAge(p2))
+console.log(p2.compareAge(p1))
+console.log(p1.compareAge(p3))
