c3ff0792286c8a32a2659f14d7121925b2bfddec
Janani Elangovan <61153365+januhub@users.noreply.github.com>
2023-06-23 10:37:01 +0530

    Create Day3.js

---
+++ Day3.js
@@ -0,0 +1,16 @@
+//Add up the Numbers from a Single Number
+
+//addUp(4) ➞ 10
+//addUp(13) ➞ 91
+//addUp(600) ➞ 180300
+
+function addUp(num) {
+    let sum = 0;
+    for (let i = 1; i <= num; i++) {
+        sum = sum + i;
+    }
+    return sum;
+}
+console.log(addUp(4));
+console.log(addUp(13));
+console.log(addUp(600));
