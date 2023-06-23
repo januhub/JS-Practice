16cbd41cb7513853c229bf25dacf2275113af5f1
Janani Elangovan <61153365+januhub@users.noreply.github.com>
2023-06-23 10:36:31 +0530

    Create Day2.js

---
+++ Day2.js
@@ -0,0 +1,15 @@
+function power(x, n) {
+    if (n == 0) {
+        return 1;
+    }
+  if (x == 0){ 
+            return 0;
+}
+    return x * power(x, n - 1);
+}
+
+console.log(power(2, 4)); // 16
+console.log(power(2, 3)); // 8
+console.log(power(2, 2)); // 4 
+console.log(power(2, 1)); // 2
+console.log(power(2, 0)); // 1
