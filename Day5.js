2721a1782966ddad3b0d43f265d207d6a0e4e4ac
Janani Elangovan <61153365+januhub@users.noreply.github.com>
2023-06-23 10:45:04 +0530

    Create Day5.js

---
+++ Day5.js
@@ -0,0 +1,17 @@
+// common multiple of 2 and 5
+for (j = 0; j <= 100; j++) {
+    if (j % 2 == 0 && j % 5 == 0) {
+        console.log(j)
+    }
+}
+
+console.log("*********************************************************************************************")
+
+//common multiple of 3 and 7
+for (j = 0; j < 100; j++) {
+    if (j % 3 == 0 && j % 7 == 0) {
+        console.log(j)
+    }
+}
+
+
