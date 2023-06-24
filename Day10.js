4a3227064ed0a4f1b4db9e14a8d46c0a138a893d
Janani Elangovan <61153365+januhub@users.noreply.github.com>
2023-06-24 17:35:00 +0530

    Square root of number

---
+++ Day10.js
@@ -0,0 +1,6 @@
+//Take input from user
+const number = prompt('Enter the number: ');
+
+//Find the square root of entered number
+const result = Math.sqrt(number);
+console.log(`The square root of ${number} is ${result}`);
