2f8261936f205974ec33858d6cf597be0ee56e4d
Janani Elangovan <61153365+januhub@users.noreply.github.com>
2023-07-04 14:35:00 +0530

    Odd or even number

---
+++ Day13.js
@@ -0,0 +1,12 @@
+// Take input from the user
+const number = prompt("Enter a number: ");
+
+//Check if the number is even
+if(number % 2 == 0) {
+    console.log("The number is even.");
+}
+
+// Check if the number is odd
+else {
+    console.log("The number is odd.");
+}
