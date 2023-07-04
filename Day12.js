aaf738a5ccc9b706eb190de09d1802f9ea99e892
Janani Elangovan <61153365+januhub@users.noreply.github.com>
2023-07-04 14:30:50 +0530

    Program to swap two variables

---
+++ Day12.js
@@ -0,0 +1,8 @@
+//Take input from the users
+let a = prompt('Enter the first variable: ');
+let b = prompt('Enter the second variable: ');
+
+[a, b] = [b, a];
+
+console.log(`The value of a after swapping: ${a}`);
+console.log(`The value of b after swapping: ${b}`);
