d8e0e8466392bb9f0ac7a07b0e4c40b2c411a3f8
Janani Elangovan <61153365+januhub@users.noreply.github.com>
2023-07-04 14:56:00 +0530

    Fibbonosi series

--- Day15.js
+++ Day15.js
@@ -1,12 +1,18 @@
-// Program to print fibonacci series up to n terms
-const number = parseInt(prompt('Enter the number of terms: '));
+const number = parseInt(prompt('Enter a positive number: '));
 let n1 = 0, n2 = 1, nextTerm;
 
 console.log('Fibonacci Series:');
+console.log(n1); // print 0
+console.log(n2); // print 1
 
-for (let i = 1; i <= number; i++) {
-    console.log(n1);
-    nextTerm = n1 + n2;
+nextTerm = n1 + n2;
+
+while (nextTerm <= number) {
+
+    // print the next term
+    console.log(nextTerm);
+
     n1 = n2;
     n2 = nextTerm;
+    nextTerm = n1 + n2;
 }
