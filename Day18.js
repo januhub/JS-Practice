e908e8d248837ab6244360286779f091b9032648
Janani Elangovan <61153365+januhub@users.noreply.github.com>
2023-07-07 11:53:36 +0530

    HCF of 2 numbers

--- Day18.js
+++ Day18.js
@@ -3,7 +3,7 @@ let hcf;
 const num1 = prompt('Enter a first positive integer: ');
 const num2 = prompt('Enter a second positive integer: ');
 
-for (let i = 1; i <= number1 && i <= number2; i++) {
+for (let i = 1; i <= num1 && i <= num2; i++) {
 
     if( num1 % i == 0 && num2 % i == 0) {
         hcf = i;
